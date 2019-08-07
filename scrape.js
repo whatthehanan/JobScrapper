438b0d979c12146f1ff9c36fcdeacffa48d9f1ab
Hanan <hannanhamza@hotmail.com>
2019-08-08 00:22:02 +0500

    refactor

--- scrape.js
+++ scrape.js
@@ -2,24 +2,26 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 request('https://news.ycombinator.com/jobs', (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
+    // check if request executed correctly
+    if (error || response.statusCode != 200)
+        console.log('error accessing website');
 
-        $('.athing').each((i, row) => {
-            let string = $(row)
-                .text()
-                .trim()
-                .toLowerCase()
-                // .split(/[^\w#]+/);
-                // .split(/(\bis hiring\b)|(\bis looking\b)|(\bhiring\b)|(\bhires\b)/);
-                // .split(/(is\s)?(hir(ing|es)|looking)+/)
-                .replace(/(is\s)?(hir(ing|es)|looking)+/, '&&&')
-                .split('&&&');
+    // load html returned by the website
+    const $ = cheerio.load(html);
 
-            console.log("Company Name:",string[0].trim())
-            console.log($(row).next().text().trim());
-        });
+    // select table row that has the jd
+    $('.athing').each((i, row) => {
+        let string = $(row)
+            .text()
+            .trim()
+            .toLowerCase()
+            .replace(/(is\s)?(hir(ing|es)|looking)+/, '&&&')
+            .split('&&&');
 
-        console.log('Scraping Done...');
-    }
+        console.log("Company Name:", string[0].trim())
+        // select next tr that has the time job was posted
+        console.log($(row).next().text().trim());
+    });
+
+    console.log('Scraping Done...');
 });
